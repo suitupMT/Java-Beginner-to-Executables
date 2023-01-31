ee6e7f4ffc0a1ba09a0fe8197472dee9b93fc2e6
suitupMT <suitup.mt@gmail.com>
2023-01-31 14:47:47 -0600

    Update Grade Example

--- 1.001 GradeCalculator/src/GradeCalculator1.java
+++ 1.001 GradeCalculator/src/GradeCalculator1.java
@@ -1,6 +1,6 @@
 //import scanner function
 import java.util.Scanner;
-
+// project example for using math and user input, more explanations going forward
 class GradeCalculator1 {
  //main method
   public static void main(String[] args) {
@@ -11,15 +11,15 @@ class GradeCalculator1 {
    int total;//total points possible
    double percentage; //final grade
    int sum; //sum of the two grades together
-   System.out.print("How many points did you get for \"Bingo\"?"); //prompt user
+   System.out.print("How many points did you get for Assignment1 of 10 possible?"); //prompt user
    grade1 = input.nextInt();//reads in first grade
 
-   System.out.print("How many points did you get for Assignment1?"); //prompt user
-   grade2 = input.nextInt();//reads in first grade
+   System.out.print("How many points did you get for Assignment2 of 10 possible?"); //prompt user
+   grade2 = input.nextInt();//reads in next grade
    
    sum = grade1 + grade2; //adding two grades
    total = 20;
-   percentage = (((double)sum)/((double)total))*100; //calculates percentage
+   percentage = (((double)sum)/((double)total))*100; //calculates percentage  
    //double type casting used on individual integer types
 
    System.out.printf("You currently have a %.0f", percentage);// float format 0 decimals
