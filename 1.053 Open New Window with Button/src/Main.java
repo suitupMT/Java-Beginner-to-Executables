7884ef1f729887669c6d0b7bac4ceb12b83e44b5
suitupMT <suitup.mt@gmail.com>
2023-02-01 17:27:52 -0600

    Added example project

---
+++ 1.053 Open New Window with Button/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// launch a page with a button using separate classes
+		
+		LaunchPage launchPage = new LaunchPage();
+
+	}
+
+}
---
+++ 1.053 Open New Window with Button/src/NewWindow.java
@@ -0,0 +1,22 @@
+import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class NewWindow {
+
+	JFrame frame = new JFrame();
+	JLabel label = new JLabel("Hello!");
+	
+	NewWindow(){
+		
+		label.setBounds(0,0,100,50);
+		label.setFont(new Font(null, Font.PLAIN, 25));
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(500,500);
+		frame.setLayout(null);
+		frame.add(label);
+		frame.setVisible(true);
+	}
+}
