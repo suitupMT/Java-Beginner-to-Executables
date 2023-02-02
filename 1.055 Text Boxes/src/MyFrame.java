5c646c2534907a1a2ac938da03af3b95541e6d7e
suitupMT <suitup.mt@gmail.com>
2023-02-01 21:08:08 -0600

    added framework

---
+++ 1.055 Text Boxes/src/MyFrame.java
@@ -0,0 +1,20 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame  implements ActionListener{
+
+	//constructor
+	MyFrame(){
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
