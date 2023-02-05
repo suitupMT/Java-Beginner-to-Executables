98c65c8b99d2c4e22625aa320e826537f965b927
suitupMT <suitup.mt@gmail.com>
2023-02-05 15:46:36 -0600

    JFileChooser Lesson

---
+++ 1.062 Select Files/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// JFileChooser is a GUI mechanism that let's a user choose a file (to open or save)
+
+		new MyFrame();
+		
+	}
+
+}
