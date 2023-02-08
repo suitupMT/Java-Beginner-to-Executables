49285bff5881655fe94a2e1dd02ac513c7f5cd96
suitupMT <suitup.mt@gmail.com>
2023-02-07 19:42:24 -0600

    command prmpt directions

--- CommandPrompt/src/Main.java
+++ CommandPrompt/src/Main.java
@@ -2,7 +2,34 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/* this will be more of a text document explaining
+		 * how to run and compile a program with the command prompt
+		 * 
+		 * 
+		 *  1. make sure your computer has a JDK installed - if eclipse is working you do
+		 *  
+		 *  2. (optional) use a text editor or notepad++ to save a file as .java
+		 *  
+		 *  3. Open Command Prompt (Windows) or Terminal (Mac/linux) on your computer
+		 *  
+		 *  4. set path=C:\Program Files\Java\jdk-19\bin (this is where your JDK is located)
+		 *  	your jdk version may be different, check to see what your number is
+		 *  
+		 *  5. cd C:\Users\(userName)\Desktop     (or whatever path the file is you want)
+		 *  
+		 *  6. javac FileName.java (to compile)
+		 *  	this will become byte code
+		 *  
+		 *  7. java FileName (to run a .class file, it's protable)
+		 *  
+		 *  
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 
+		 */
 
 	}
 
