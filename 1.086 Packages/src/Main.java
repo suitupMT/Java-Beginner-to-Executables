71d5bebd81bc1bb7c457807d40eca98762792f40
suitupMT <suitup.mt@gmail.com>
2023-02-07 19:04:30 -0600

    lesson

---
+++ 1.086 Packages/src/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.Icon; //this is a typical package downloaded from the JRE system library
+import Tools.ToolBox; // we imported our own package
+
+public class Main {
+
+	public static void main(String[] args) {
+		// package is a bundle of related files that we can use in programs
+		
+		ToolBox toolbox = new ToolBox();
+		
+		// this is just the basics but might come in handy as you need
+		// to build your own package or library and use it
+	}
+
+}
